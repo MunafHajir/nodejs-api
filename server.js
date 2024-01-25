8eccf53469b4268a06da1699d31ca0272b053e91
Munaf Hajir <munafhajirpro@gmail.com>
2024-01-25 15:13:25 +0530

    GET APIs

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mysql = require("mysql2");
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -11,40 +11,85 @@ const connection = mysql.createConnection({
 });
 
 const app = express();
-app.use(bodyParser.json())
-
+app.use(bodyParser.json());
 
 const getUsers = async (req, res, next) => {
   try {
-      const [results] = await connection.promise().query("SELECT * FROM users");
-      res.send();
-  } catch(err) {
-      console.log(err)
+    const { limit, offset } = req.query;
+
+    const queryData = req.query;
+    let phoneNo = queryData.phone_no;
+    if (!phoneNo) {
+      res.status(400).send({ message: "phone_no is required" });
+    }
+
+    const queryString =
+      "SELECT email, phone_no, is_active, created_at FROM users WHERE phone_no = ? LIMIT ? OFFSET ?";
+    const [results] = await connection
+      .promise()
+      .execute(queryString, [phoneNo, limit, offset]);
+    const countQueryString = "SELECT COUNT(*) as count FROM users";
+    const [countResults] = await connection.promise().execute(countQueryString);
+
+    const responseBody = {
+      message: "Users list",
+      list: results,
+      count: countResults[0].count,
+    };
+
+    res.status(200).send(responseBody);
+  } catch (err) {
+    res.status(500).send({ message: "Internal Server Error" });
   }
-}
+};
 
 const createUser = async (req, res, next) => {
   const { id, email, phone_no, passward, is_active, created_at } = req.body;
 
   const queryString = `
   insert into users 
-  (id, email, phone_no, passward, is_active, created_at)
-  values (${id}, "${email}", ${phone_no}, "${passward}", ${is_active}, "${created_at}");
-  `
+  (email, phone_no, passward, is_active)
+  values ( ?, ?, ?, ?);
+  `;
 
-  const [results] = await connection.promise().query(queryString);
-  
+  const [results] = await connection
+    .promise()
+    .execute(queryString, [email, phone_no, passward, is_active]);
+
   res.status(201).send({
     message: "Users added successfully",
-    results
-  })
-}
+    results,
+  });
+};
 
+const getUserDetails = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    //validation
+    const queryString =
+      "SELECT email, phone_no, is_active, created_at FROM users WHERE id = ?";
+    const [results] = await connection
+      .promise()
+      .execute(queryString, [id]);
+    console.log(results);
+    if(!results || results.length === 0){
+      res.status(404).send({message: "User not found"});
+    }
 
+    const responseBody = {
+      message: "Users list",
+      details: results[0]
+    };
+
+    res.status(200).send(responseBody);
+  } catch (err) {
+    res.status(500).send({ message: "Internal Server Error" });
+  }
+};
 
 // Users Api
 app.get("/users", getUsers);
+app.get("/users/:id", getUserDetails);
 app.post("/users", createUser);
-
 
 app.listen(3000, console.log("server started"));
