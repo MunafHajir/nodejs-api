b5161aa7387062b64e9733fe6dfc0a8bf88bb3f8
Munaf Hajir <munafhajirpro@gmail.com>
2024-02-13 16:11:04 +0530

    Added Cors

--- server.js
+++ server.js
@@ -1,20 +1,27 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const cors = require("cors");
 const uuid = require("uuid").v4;
-const v1 = require('./apis/v1');
+
+const v1 = require("./apis/v1");
 
 const app = express();
 app.use(bodyParser.json());
 
+const options = {
+  origin: "*",
+  methods: "GET,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
+app.use(cors(options));
 //application level middleware
 app.use((req, res, next) => {
   req.headers["request_id"] = uuid();
-  next()
+  next();
 });
 
-
 // private apis
-app.use('/v1', v1);
+app.use("/v1", v1);
 
 app.listen(3000, console.log("server started"));
